59a23a522011c16c359f1501bf7af68e25a8b0c6
Zachary Wells <8bitslime@gmail.com>
2020-08-27 23:17:09 -0700

    Created generic traits module
    Created a generic traits module to hold all traits which aren't necessarily
    for integers or floating point numbers.

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@ pub mod traits;
 #[rustfmt::skip]
 pub mod prelude {
     pub use crate::{
+        traits::*,
         traits::float::*,
+        traits::generic::*,
         traits::integer::*,
-        traits::*
     };
 }
--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,33 +1,20 @@
 use core::iter::*;
 use core::ops::*;
+use core::fmt::Debug;
 
+pub mod generic;
 pub mod float;
 pub mod integer;
 
-pub trait Signed: Sized + Neg<Output = Self> {
-    fn abs(&self) -> Self;
-    fn signum(&self) -> Self;
-}
-
-pub trait Identity {
-    fn identity() -> Self;
-}
-
-pub trait Zero {
-    fn zero() -> Self;
-}
-
-impl<N: Numeric + Default> Zero for N {
-    #[inline(always)]
-    fn zero() -> Self { Self::default() }
-}
+pub use generic::{Identity, Signed, Zero};
 
 pub trait Numeric:
-    Sized + Clone + PartialEq + PartialOrd
+    Sized + Debug + Clone + Copy
+    + PartialEq + PartialOrd
     + Sum<Self> + Product<Self>
     + Add<Self, Output = Self> + AddAssign<Self>
     + Div<Self, Output = Self> + DivAssign<Self>
     + Mul<Self, Output = Self> + MulAssign<Self>
     + Sub<Self, Output = Self> + SubAssign<Self>
     + Rem<Self, Output = Self> + RemAssign<Self>
-    + Zero + Identity {}
+    + Identity + Zero {}
